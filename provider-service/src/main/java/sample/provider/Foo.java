8fbe8d38f2b0dffe9431ffcf76e9634cc11ff1e5
Spencer Gibb <spencer@gibb.us>
2015-08-17 12:40:41 -0600

    remove pact branding

---
+++ provider-service/src/main/java/sample/provider/Foo.java
@@ -0,0 +1,18 @@
+package sample.provider;
+
+public class Foo {
+
+    private int value;
+
+    public Foo(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
---
+++ provider-service/src/test/groovy/sample/provider/BaseMockMvcSpec.groovy
@@ -0,0 +1,14 @@
+package sample.provider
+
+import com.jayway.restassured.module.mockmvc.RestAssuredMockMvc
+import spock.lang.Specification
+
+/**
+ * @author Spencer Gibb
+ */
+abstract class BaseMockMvcSpec extends Specification {
+
+    def setup() {
+        RestAssuredMockMvc.standaloneSetup(new Application())
+    }
+}
