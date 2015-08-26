8dfebbe25aadc97938a7489c954abfd8b55c0812
Spencer Gibb <spencer@gibb.us>
2015-08-26 15:45:11 -0600

    move Foo to lombok

--- fooconsumer/src/test/java/sample/consumer/ConsumerApplicationTests.java
+++ fooconsumer/src/test/java/sample/consumer/ConsumerApplicationTests.java
@@ -3,12 +3,12 @@ package sample.consumer;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = ConsumerApplication.class)
-@WebAppConfiguration
+@WebIntegrationTest(randomPort = true)
 public class ConsumerApplicationTests {
 
 	@Test
--- fooprovider/src/main/java/sample/provider/Foo.java
+++ fooprovider/src/main/java/sample/provider/Foo.java
@@ -1,18 +1,12 @@
 package sample.provider;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Foo {
-
 	private int value;
-
-	public Foo(int value) {
-		this.value = value;
-	}
-
-	public int getValue() {
-		return value;
-	}
-
-	public void setValue(int value) {
-		this.value = value;
-	}
 }
